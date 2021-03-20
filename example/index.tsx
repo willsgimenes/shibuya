b9e7b23cb705b51bae4e2d23586ab0da04ff6c44
willsgimenes <willsgimenes@gmail.com>
2021-03-20 01:56:53 -0300

    feat(card): adds card to playground

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,20 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Button, Card } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Button text="Olá mundo" />
+      <Card style={{ width: '600px' }}>
+        <h1>Title</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda,
+          cum eum impedit, in inventore ipsa ipsam quae quam qui ut vel
+          voluptatem. Ad, aliquam debitis minus sit sunt voluptate?
+        </p>
+      </Card>
     </div>
   );
 };
