08fe591cceea17f8c589b71e7fa363d3cf9cdc2b
willsgimenes <willsgimenes@gmail.com>
2021-03-21 18:41:35 -0300

    feat(input): adding input component
    fix #6

---
+++ src/components/Input.tsx
@@ -0,0 +1,41 @@
+import React, { FC, useRef, InputHTMLAttributes } from 'react';
+import styled from 'styled-components';
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  innerRef: any;
+  disabled?: boolean;
+  label: string;
+}
+
+const StyledInput = styled.input.attrs(
+  ({ disabled }: { disabled: boolean }) => ({
+    className: `p-2 border rounded-lg placeholder-yellow-800 ${disabled &&
+      'border-gray-100 text-gray-400'} `,
+  })
+)``;
+
+export const Input: FC<InputProps> = ({
+  value,
+  onChange,
+  innerRef,
+  disabled = false,
+  label,
+  ...props
+}) => {
+  const inputRef = innerRef || useRef(null);
+
+  return (
+    <fieldset>
+      <label htmlFor={label}>{label}</label>
+      <StyledInput
+        id={label}
+        value={value}
+        type="text"
+        onChange={onChange}
+        ref={inputRef}
+        disabled={disabled}
+        {...props}
+      />
+    </fieldset>
+  );
+};
