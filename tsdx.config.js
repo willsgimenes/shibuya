5b5bcf653a58c4a986912a9ad901aeb8b3946dde
willsgimenes <willsgimenes@gmail.com>
2021-03-24 19:48:55 -0300

    feat(deps): lean the project with correct deps
    fix #16

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,8 +1,10 @@
 const postcss = require('rollup-plugin-postcss');
+const peerDepsExternal = require('rollup-plugin-peer-deps-external');
 
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
+      peerDepsExternal(),
       postcss({
         config: {
           path: './postcss.config.js',
